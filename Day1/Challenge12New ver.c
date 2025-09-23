99a0ec4306f60cc46acdc37861dc2ee7427d3b08
Charaf-Ycode <c.chatoui01@gmail.com>
2025-09-23 11:29:44 +0200

    add Challnges

---
+++ Day1/Challenge12New ver.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("Saisie votre 4 Nombres:");
+    scanf( "%d %d %d %d", &a, &b, &c, &d);
+    printf("Votre Nombre en inverse est:%d %d %d %d", d, c, b, a);
+    return 0;
+}
